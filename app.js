c55db19331c7952e3c7a0856bcddcf3ec3e9ade3
Liam Ilan <snowboardsheep@gmail.com>
2018-12-29 15:48:44 -0800

    Implemented routes for the Monty Hall Problem (new game, initial choice, switch). Database is mocked through an array of objects. Implemented a mechanism to randomly choose an eliminated door.

--- app.js
+++ app.js
@@ -46,7 +46,88 @@ const app = express()
 // use the body parser
 app.use(bodyParser.json())
 
-// save the bublic directory
+// save the public directory
 app.use(express.static('public'))
 
+let games = []
 
+// this function chooses a random door using the built in node.js random number generator
+function chooseRandomDoor () {
+  return Math.floor(Math.random() * 3)
+}
+
+// this function eliminates a door
+function eliminateDoor (userChoice, prizePosition) {
+  // filter out userChoice and prizePosition
+  const possible = [0, 1, 2].filter(function (item, i) {
+    return !(item === userChoice || item === prizePosition)
+  })
+
+  // return a random item from the remaining array
+  // if we have an array with a length of 1 (the user got their guess wrong),
+  // then we just return the remaining item
+  // if we have an array with a length of 2 (the user got their guess right),
+  // then we return a random pick of the two remaining elements in the array
+  return possible[Math.floor(Math.random() * possible.length)]
+}
+
+app.post('/v1/game/new', function (req, res) {
+  let game = {}
+
+  // the time the experiment was created in milliseconds since January 1, 1970, 00:00:00 UTC
+  game.created_at = Date.now()
+
+  // id of experiment
+  game._id = Math.random()
+
+  // position of the prize
+  game.position_of_prize = chooseRandomDoor()
+
+  // push the game into the Database
+  games.push(game)
+
+  // ONLY return _id
+  res.json({ _id: game._id })
+
+  console.log(games)
+})
+
+app.put('/v1/game/choice', function (req, res) {
+  // a mock of finding the game in the db
+  let game = games.find(item => item._id === req.body._id)
+
+  // the time the user makes the choice in milliseconds since January 1, 1970, 00:00:00 UTC
+  game.choice_at = Date.now()
+
+  // the user's choice of doors
+  game.initial_choice = req.body.choice
+
+  // the door we eliminate
+  game.eliminated_door = eliminateDoor(game.initial_choice, game.position_of_prize)
+
+  // a mock of finding the game in the database, and THEN replacing it with our new game
+  games.forEach(function (item, i) {
+    if (item._id === game._id) {
+      games[i] = game
+    }
+  })
+
+  console.log(game)
+
+  // ONLY return eliminated door
+  res.json({ eliminated_door: game.eliminated_door })
+})
+
+app.put('/v1/game/switch', function (req, res) {
+  // a mock of finding the game in the db
+  let game = games.find(item => item._id === req.body._id)
+
+  game.switch_at = Date.now()
+  game.final_choice = req.body.final_choice
+  game.win = game.final_choice === game.position_of_prize
+  game.switched = game.final_choice === game.initial_choice
+
+  console.log(game)
+
+  res.json({ position_of_prize: game.position_of_prize, win: game.win })
+})
