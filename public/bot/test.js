f92e6dbf1f3a3cf68f3e68f2ccb4b5870125265d
Liam Ilan <snowboardsheep@gmail.com>
2019-01-04 12:55:03 -0800

    Added a bot to loop over the experiment.

---
+++ public/bot/test.js
@@ -0,0 +1,47 @@
+/* global api */
+
+function runGame () {
+  // the game data we collect
+  let game = {}
+
+  // make a new game
+  api.newGame(function (id) {
+    // the game id
+    game._id = id._id
+
+    // pick the first choice
+    let choice = Math.floor(Math.random() * 3)
+
+    // make a choice
+    api.makeChoice(game._id, choice, function (eliminated) {
+      // record the choice and the eliminated door
+      game.choice = choice
+      game.eliminated = eliminated.eliminated_door
+
+      // the door to switch to
+      let switchTo = [0, 1, 2]
+
+      switchTo.splice(switchTo.indexOf(game.eliminated), 1)
+
+      switchTo = switchTo[Math.floor(Math.random() * 2)]
+
+      api.changeChoice(switchTo, game._id, function (res) {
+        // record the final data
+        game.switchedTo = switchTo
+        game.posOfPrize = res.position_of_prize
+        game.win = res.win
+
+        // log the collected data
+        console.log(game)
+      })
+    })
+  })
+}
+
+function loopGames(amount){
+  for (let i = 0; i < amount; i++) {
+    runGame()
+  };
+}
+
+
